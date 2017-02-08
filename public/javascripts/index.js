b6fc755a09b524d05ae7942524dd5dd8b7b4e01f
עידו שתיוי < >
2017-02-08 14:41:00 +0200

    Navbar created

---
+++ public/javascripts/index.js
@@ -0,0 +1,12 @@
+app.controller("mainCtrl", ['$rootScope', '$scope', function($rootScope, $scope) {
+    $scope.mainMenu = [{
+        description: "עמוד ראשי",
+        link: ""
+    }, {
+        description: "העבודות שלנו",
+        link: ""
+    }, {
+        description: "חוות דעת",
+        link: ""
+    }]
+}])
