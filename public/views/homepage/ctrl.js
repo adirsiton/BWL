8fece8224aa92824c7c9d8537473c7c9f05120b5
Adir Sation <adirush120@gmail.com>
2017-02-08 16:44:13 +0200

    adding login to facebook

--- public/views/homepage/ctrl.js
+++ public/views/homepage/ctrl.js
@@ -1,3 +1,34 @@
-app.controller("homepageCtrl", ['$scope', function($scope) {
+app.controller("homepageCtrl", ['$scope', function ($scope) {
     $scope.currentNavItem = 'main';
+
+    $scope.getLoginStatus = function () {
+        FB.getLoginStatus(function (response) {
+            if(response.status != 'connected') {
+                $scope.logintoFB(response);
+            }
+        });
+    }
+
+    $scope.logintoFB = function () {
+        FB.login(function (response) {
+            if (response.status === 'connected') {
+                // Logged into your app and Facebook.
+            } else if (response.status === 'not_authorized') {
+                // The person is logged into Facebook, but not your app.
+            } else {
+                // The person is not logged into Facebook, so we're not sure if
+                // they are logged into this app or not.
+            }
+        });
+    }
+
+    $scope.logoutFB = function () {
+        FB.logout(function (response) {
+          // Logged out
+        });
+    }
+
+    setTimeout(function () {
+        $scope.getLoginStatus();
+    }, 100);
 }])
