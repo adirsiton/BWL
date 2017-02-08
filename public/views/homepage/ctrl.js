cc714440a8eb181cc94ed032ec020299ee3fe337
עידו שתיוי < >
2017-02-08 19:57:13 +0200

    highlights cool effect

--- public/views/homepage/ctrl.js
+++ public/views/homepage/ctrl.js
@@ -34,10 +34,14 @@ app.controller("homepageCtrl", ['$scope', function ($scope) {
 
     // Listen to latest work image 
     var latestWorksImgEvents = function() {
+        $(".highlight").css("visibility", "hidden");
         $(".latest-works img").on("mouseover", function(e) {
             $(this).siblings(".highlight").addClass("active");
+            $(this).siblings(".highlight").removeClass("leave");
+            $(".highlight").css("visibility", "visible");
         }).on("mouseleave", function(e) {
             $(this).siblings(".highlight").removeClass("active");
+            $(this).siblings(".highlight").addClass("leave");
         });
     };
     latestWorksImgEvents();
