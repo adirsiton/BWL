1a110003789cf00f38549b0a1a8c9358c7fc071c
עידו שתיוי < >
2017-03-09 15:12:12 +0200

    doesn't let adding a new work if the name is already exist

--- public/views/works/new-work/ctrl.js
+++ public/views/works/new-work/ctrl.js
@@ -19,12 +19,16 @@ app.controller("newWorkCtrl", function($scope, $mdDialog, worksApi) {
             $scope.saving = true;
 
             // Save the work
-            worksApi.addWork($scope.work).then(function() {
+            worksApi.addWork($scope.work).then(function(res) {
                 $scope.saved = true;
                 $scope.saving = false;
                 $scope.dropzone = new Dropzone("#dropzone", {
                     url: "/api/works/upload"
                 }); 
+            }).catch(function(err) {
+                $scope.saving = false;
+                $scope.saved = false;
+                swal("לא ניתן להוסיף את העבודה", err.data, "error");
             });
         }  
     }
